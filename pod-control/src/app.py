33ed0697a5a360bbbe2587a39f41b15af0d4c00c
Taesung Hwang <taesungh@uci.edu>
2023-05-22 22:01:42 -0700

    Refactor FSM into class to handle socket events
    - Register event handlers by binding `on_*` methods to socket server
    - Prefer composition over inheritance

--- pod-control/src/app.py
+++ pod-control/src/app.py
@@ -1,11 +1,14 @@
 import socketio
 
+from fsm import FSM
 from services.pod_socket_server import PodSocketServer
 
 sio = socketio.AsyncServer(
     async_mode="asgi", cors_allowed_origins="http://localhost:3000"
 )
 
-sio.register_namespace(PodSocketServer("/pod"))
+pod_socket = PodSocketServer("/pod")
+sio.register_namespace(pod_socket)
 
 app = socketio.ASGIApp(sio)
+fsm = FSM(pod_socket)
--- pod-control/src/fsm.py
+++ pod-control/src/fsm.py
@@ -1,7 +1,34 @@
 import asyncio
+from typing import Callable, Coroutine, Mapping
+
+from services.pod_socket_server import PodSocketServer
+
+EventHandler = Callable[..., None | Coroutine[None, None, None]]
 
 
-async def run_periodic() -> None:
-    while True:
-        print("tick")
-        await asyncio.sleep(1)
+class FSM:
+    def __init__(self, socket_server: PodSocketServer):
+        """Store socket server instance."""
+        self.socket = socket_server
+        self._register_handlers(
+            {
+                "stop": self.handle_stop,
+            }
+        )
+
+        self.running = True
+
+    async def run_periodic(self) -> None:
+        while self.running:
+            print("tick")
+            await asyncio.sleep(1)
+
+    async def handle_stop(self, sid: str) -> None:
+        """Stop the FSM and the pod."""
+        self.running = False
+        # TODO: actually stop the pod
+
+    def _register_handlers(self, handlers: Mapping[str, EventHandler]) -> None:
+        """Register given handlers as socket event handlers."""
+        for event, handler in handlers.items():
+            setattr(self.socket, f"on_{event}", handler)
