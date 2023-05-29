700f238ed541129877d7717e89abf97c7707f013
Taesung Hwang <taesungh@uci.edu>
2023-05-28 18:43:25 -0700

    Add "enter actions" to FSM for new transitions
    - When entering a different state than the previous run "enter actions"
    - Inspired by FRC Iterative Robot

--- pod-control/src/fsm.py
+++ pod-control/src/fsm.py
@@ -25,10 +25,20 @@ class FSM:
             }
         )
 
-        self._state = State.INIT
+        self._state: State = State.INIT
+        self._last_state = self._state
         self._running_tick = 0
+        # State transitions are run whenever the pod is in the given state
+        # These transitions should return the next state
         self._state_transitions = {
             State.RUNNING: self._running_periodic,
+        }
+
+        # Enter actions are run once when transitioning to a state from a different one
+        self._enter_actions = {
+            State.SERVICE: self._enter_service,
+            State.RUNNING: self._enter_running,
+            State.STOPPED: self._enter_stopped,
         }
 
         # To avoid race conditions, socket handlers will defer state transitions
@@ -42,10 +52,16 @@ class FSM:
 
     def tick(self) -> None:
         """Tick the state machine by running the action for the current state."""
+        # If entering a new state, run the enter action
+        if self._state != self._last_state and self._state in self._enter_actions:
+            self._enter_actions[self._state]()
+
         self._pod_periodic()
         next_state = self._state
         if self._state in self._state_transitions:
             next_state = self._state_transitions[self._state]()
+
+        self._last_state = self._state
         # This should be the only place where a value is assigned to self._state
         self._state = self._interrupt_state or next_state
         self._interrupt_state = None
@@ -54,10 +70,22 @@ class FSM:
         """Perform operations on every tick."""
         pass
 
+    def _enter_service(self) -> None:
+        """Perform operations once when entering the service state."""
+        pass
+
+    def _enter_running(self) -> None:
+        """Perform operations once when starting to run the pod."""
+        pass
+
     def _running_periodic(self) -> State:
         """Perform operations when the pod is running."""
         self._running_tick += 1
         return State.RUNNING
+
+    def _enter_stopped(self) -> None:
+        """Perform operations once when stopping the pod."""
+        pass
 
     async def handle_start(self, sid: str) -> None:
         """Start the FSM and the pod."""
